d19565d1b3469883433555b5d3683755c1e75a49
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-10-09 21:10:20 -0300

    Database configuration.

--- src/copper/config/database.py
+++ src/copper/config/database.py
@@ -1,13 +1,13 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from .settings_extractor import settings_parser
+
+settings = settings_parser()["database"]
 
 engine = create_engine(
-    os.getenv("SQLALCHEMY_DATABASE_URI"),
-    echo=bool(os.getenv("SQLALCHEMY_DATABASE_ECHO")),
+    settings["sqlalchemy_database_uri"], echo=settings["sqlalchemy_database_echo"],
 )
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
