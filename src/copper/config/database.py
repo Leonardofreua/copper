248f34125dba316c8a55d15f286c415a1fc4592b
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-10-16 20:37:44 -0300

    Settings centralized in the scrapy settings.py file.

--- src/copper/config/database.py
+++ src/copper/config/database.py
@@ -1,14 +1,15 @@
+from scrapy.utils import project
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from .settings_extractor import settings_parser
 
-settings = settings_parser()["database"]
+settings = project.get_project_settings()
 
-engine = create_engine(
-    settings["sqlalchemy_database_uri"], echo=settings["sqlalchemy_database_echo"],
-)
+database_path = settings.attributes["SQLALCHEMY_DATABASE_URI"].value
+is_echo = settings.attributes["SQLALCHEMY_DATABASE_ECHO"].value
+
+engine = create_engine(database_path, echo=is_echo)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
